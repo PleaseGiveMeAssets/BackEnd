88d7459bce130bae757bd75744e707edcdef9134
JeongHyunBaek <bjhbhm@gmail.com>
2024-09-07 13:27:30 +0900

    init commit

---
+++ src/main/java/com/example/spring/vo/ExampleVO.java
@@ -0,0 +1,12 @@
+package com.example.spring.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExampleVO {
+    private String example;
+}
