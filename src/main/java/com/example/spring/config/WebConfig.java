19a3ce43fa47a3086ec6497c1ed24b4dc021ac7f
JeongHyunBaek <69346978+JeongHyunBaek@users.noreply.github.com>
2024-10-02 09:27:06 +0900

    Merge pull request #36 from PleaseGiveMeAssets/hotfix/gitignore
    chore : gitignore 전체 수정

--- src/main/java/com/example/spring/config/WebConfig.java
+++ src/main/java/com/example/spring/config/WebConfig.java
@@ -2,7 +2,6 @@ package com.example.spring.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,16 +9,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 @ComponentScan(basePackages = "com.example.spring")
 public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
-                .allowedOrigins("http://localhost:5173")//수정필요!
-                .allowedMethods("*") // 모든 HTTP 메서드 허용
-                .allowedHeaders("*") // 모든 헤더 허용
-                .allowCredentials(true); // 쿠키 같은 자격 증명 허용
-    }
-
 }
 
 
