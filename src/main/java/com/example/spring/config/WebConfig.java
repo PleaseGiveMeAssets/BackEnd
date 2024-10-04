8962cabad32228500cf5101fe1ff7cf047af9fe7
Kwon9302 <148173045+Kwon9302@users.noreply.github.com>
2024-10-04 13:49:34 +0900

    feat : portfolio 차트 그리기

--- src/main/java/com/example/spring/config/WebConfig.java
+++ src/main/java/com/example/spring/config/WebConfig.java
@@ -11,18 +11,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 @ComponentScan(basePackages = "com.example.spring")
 public class WebConfig implements WebMvcConfigurer {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/v1/**")
-                        .allowedOrigins("http://localhost:8080", "http://localhost:8081", "http://localhost:5173") // Adjust as needed
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
-    }
+
 }
 
