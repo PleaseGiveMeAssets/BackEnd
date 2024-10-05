7946423b7f1af0c7663924a86b35ca35dba8a091
S2gamzaS2 <ipnsohe@naver.com>
2024-10-05 17:51:15 +0900

    feature : 로그인 기능 구현

--- src/main/java/com/example/spring/config/SecurityConfig.java
+++ src/main/java/com/example/spring/config/SecurityConfig.java
@@ -1,12 +1,19 @@
 package com.example.spring.config;
 
+import com.example.spring.security.filter.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -22,14 +29,29 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public UserDetailsService userDetailsService() {
+        return username -> {
+            if ("testUser".equals(username)) {
+                return User.withUsername("testUser")
+                        .password(passwordEncoder().encode("testPassword")) // 비밀번호 해시화
+                        .roles("USER") // 권한 설정
+                        .build();
+            }
+            throw new UsernameNotFoundException("User not found");
+        };
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
         http
-                .cors(cors -> cors.configurationSource(corsConfigurationSource())) // CORS 설정
-                .csrf((csrf) -> csrf.disable()) // CSRF 비활성화
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/**").permitAll() // 해당 경로에 대한 접근 허용
-                        .anyRequest().authenticated() // 그 외 요청은 인증 필요
-                );
+                        .requestMatchers("/", "/api/v1/auth/**", "/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
 
@@ -44,5 +66,11 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration); // 모든 경로에 대해 CORS 설정
         return source;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
--- src/main/java/com/example/spring/service/MemberService.java
+++ src/main/java/com/example/spring/service/MemberService.java
@@ -2,6 +2,7 @@ package com.example.spring.service;
 
 import com.example.spring.dto.FindIdRequestDTO;
 import com.example.spring.dto.FindPasswordRequestDTO;
+import com.example.spring.dto.LoginRequestDTO;
 import com.example.spring.dto.MemberDTO;
 
 import java.util.Map;
@@ -15,4 +16,10 @@ public interface MemberService {
 
     // 비밀번호 찾기
     int findPassword(FindPasswordRequestDTO findPasswordRequestDTO);
+
+    // 로그인
+    String login(LoginRequestDTO loginRequest);
+
+    // 로그아웃
+    void logout(String token);
 }
