d995cea8faaa0ebd3dc7d20f508d47a3f06bc21b
heebp <skdnfl4343@gmail.com>
2024-10-07 20:09:33 +0900

    refactor: 변경된 로직에 따라 수정

--- src/test/java/com/example/spring/service/StockPortfolioServiceTest.java
+++ src/test/java/com/example/spring/service/StockPortfolioServiceTest.java
@@ -1,6 +1,7 @@
 package com.example.spring.service;
 
 import com.example.spring.config.AppConfig;
+import com.example.spring.dto.ForChartDTO;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,12 +18,12 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @WebAppConfiguration
 public class StockPortfolioServiceTest {
     @Autowired
-    private StockPortfolioService stockPortfolioService;
+    private PortfolioService stockPortfolioService;
 
     @Test
     public void getStockPortfolioInfo() {
         String userId = "testUser1";
-        List<StockPortfolioDTO> stockPortfolioDTOList = stockPortfolioService.getStockPortfolioInfo(userId);
-        assertTrue(stockPortfolioDTOList.size() > 0);
+        List<ForChartDTO> orderList = stockPortfolioService.getOrderList(userId);
+        assertTrue(!orderList.isEmpty());
     }
 }
